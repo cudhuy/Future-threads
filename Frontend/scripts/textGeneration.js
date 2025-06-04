d9bc5a6d21a7784a826d453c472280a6acace56e
Tran Huy <tdhuy141vt@gmail.com>
2025-06-05 00:05:45 +0700

    adding scripts

---
+++ Frontend/scripts/textGeneration.js
@@ -0,0 +1,9 @@
+/* 
+	category: string, what type of thing to generate
+	options: {optimism: int 0-10, genres: [{name: string, amount: int 0-10}}]
+	returns: string of the text
+*/
+function generateText(category, options) {
+	return 'todo';
+}
+export default generateText;
